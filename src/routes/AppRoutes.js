b3f4ea149c181f2351c9f81df40ba80f43828969
CarlinGebyte <91474067+CarlinGebyte@users.noreply.github.com>
2022-03-26 12:56:23 -0500

    Creating project

---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function AppRoutes() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default AppRoutes;
